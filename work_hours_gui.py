444a8dd0a63fc112e91c7a7dd4e8cf9e3eb940c0
Chris Kavanagh <ckava3@gmail.com>
2015-02-11 00:52:32 -0500

    02-11-2015 corrected method add_data, working copy

--- work_hours_gui.py
+++ work_hours_gui.py
@@ -43,15 +43,15 @@ class GUI:
         age_label.grid(row=1, sticky=W)
         
         
-        name_var = StringVar()
-        e1 = Entry(frame, textvariable=name_var)
+        self.name_var = StringVar()
+        e1 = Entry(frame, textvariable=self.name_var)
         e1.grid(row=0, column=1)
-        name = name_var.get()
+        #self.name = name_var.get()
 
-        age_var =  IntVar()
-        e2 = Entry(frame, textvariable=age_var)
+        self.age_var =  IntVar()
+        e2 = Entry(frame, textvariable=self.age_var)
         e2.grid(row=1, column=1)
-        age = age_var.get()
+        #self.age = age_var.get()
 
         add_button = Button(frame, text="Add", command=self.add_data)
         add_button.grid(row=2, column=2)
@@ -60,6 +60,8 @@ class GUI:
         quit_button.grid(row=2, column=3)
 
     def add_data(self):
+        name = self.name_var.get()
+        age = self.age_var.get()
         new_person = Test(name=name, age=age)
         session.add(new_person)
         session.commit()
@@ -72,50 +74,3 @@ root = Tk()
 root.title("Work-Hours-GUI")
 mygui = GUI(root)
 root.mainloop()
-
-
-##
-##from datetime import datetime
-##from tkinter import *
-##
-##root = Tk()
-##
-### create master frame with title
-##master = Frame(root, width=768, height=576)
-##root.title("Work Hours GUI")
-##master.pack()
-##
-##
-### create date string
-##d = datetime.today()
-##date = d.strftime("%Y-%m-%d")
-##
-##
-### create labels and entries
-##label_add = Label(master, text="Add Hours? ")
-##label_add.grid(row=0)
-##entry_add = Entry(master)
-##entry_add.grid(row=0, column=1)
-##
-##label_sub = Label(master, text="Subtract Hours? ")
-##label_sub.grid(row=1)
-##entry_sub = Entry(master)
-##entry_sub = entry_sub.grid(row=1, column=1)
-##
-##label_notes = Label(master, text="Add Notes? ")
-##label_notes.grid(row=2)
-##entry_notes = Entry(master)
-##entry_notes.grid(row=2, column=1)
-##
-##label_show_hours = Label(master, text="Total Hours: ")
-##label_show_hours = label_show_hours.grid(row=3)
-##entry_show_hours = Entry(master)        #this needs to be changed to Text()
-##entry_show_hours.grid(row=3, column=1)  #this needs to be changed to Text()
-##
-### text.delete(0.0, END)
-### text.insert(0.0, message)
-##
-##
-##
-##
-##master.mainloop()
