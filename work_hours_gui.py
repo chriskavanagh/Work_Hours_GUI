470893db89177d8ffa96ffeef100281f17f1f92c
Chris Kavanagh <ckava3@gmail.com>
2015-02-13 06:05:23 -0500

    02-13-2015 added bottom frame, etc

--- work_hours_gui.py
+++ work_hours_gui.py
@@ -33,8 +33,22 @@ session = Session()
 class GUI:
     def __init__(self, parent):
         self.parent = parent
+
+        # create top frame
         frame = Frame(parent)
-        frame.pack(expand=YES, fill=BOTH)
+        frame.pack(expand=YES, fill=BOTH, padx=5, pady=5)
+
+        # create bottom frame
+        btm_frame = Frame(parent, relief=SUNKEN, borderwidth=1)
+        btm_frame.pack(side=BOTTOM, expand=YES, fill=BOTH, padx=5, pady=5)
+
+        # create datetime object
+        d = datetime.today()
+        date = d.strftime("%Y-%m-%d")
+
+        # create label to display date
+        date_label = Label(btm_frame, text=date)
+        date_label.pack(side=LEFT)
 
         name_label = Label(frame, text="Enter Name:")
         name_label.grid(row=0, sticky=W)
@@ -53,13 +67,18 @@ class GUI:
         e2.grid(row=1, column=1)
         
 
-        add_button = Button(frame, text="Add", command=self.add_data)
-        add_button.config(padx=5, pady=5)
-        add_button.grid(row=2, column=2)
+        quit_button = Button(btm_frame, text="Quit", command=parent.destroy)
+        quit_button.pack(side=RIGHT)
+##        quit_button.config(padx=5, pady=5)
+##        quit_button.grid(row=2, column=3)
+        
 
-        quit_button = Button(frame, text="Quit", command=parent.destroy)
-        quit_button.config(padx=5, pady=5)
-        quit_button.grid(row=2, column=3)
+        add_button = Button(btm_frame, text="Add", command=self.add_data)
+        add_button.pack(side=RIGHT)
+##        add_button.config(padx=5, pady=5)
+##        add_button.grid(row=2, column=2)
+
+        
 
     def add_data(self):
         name = self.name_var.get()
@@ -73,7 +92,7 @@ class GUI:
 
 
 root = Tk()
-#root.geometry("350x400")
+root.geometry("250x100")
 root.title("Work-Hours-GUI")
 mygui = GUI(root)
 root.mainloop()
