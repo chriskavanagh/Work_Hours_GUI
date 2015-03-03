d4d7414e4abe75f44808ebf8e93f23592c7c0656
Chris Kavanagh <ckava3@gmail.com>
2015-03-03 07:14:29 -0500

    Added clear_data method

--- test_gui2.py
+++ test_gui2.py
@@ -54,7 +54,7 @@ class GUI:
         date = d.strftime("%Y-%m-%d")
         
 
-#------------------------Create Labels-----------------------------#
+#------------------------Create Labels-------------------------------#
         
 
         # create label to display date
@@ -103,48 +103,48 @@ class GUI:
             
         # create name variable and entry
         self.name_var = StringVar()
-        e1 = Entry(frame, textvariable=self.name_var)
-        e1.grid(row=0, column=1)
+        self.e1 = Entry(frame, textvariable=self.name_var)
+        self.e1.grid(row=0, column=1)
          
-        # creat age variable and entry
+        # create age variable and entry
         self.age_var =  IntVar()
-        e2 = Entry(frame, textvariable=self.age_var)
-        e2.grid(row=1, column=1)
+        self.e2 = Entry(frame, textvariable=self.age_var)
+        self.e2.grid(row=1, column=1)
 
         # create address variable and entry
         self.address_var = StringVar()
-        e3 = Entry(frame, textvariable=self.address_var)
-        e3.grid(row=2, column=1)
+        self.e3 = Entry(frame, textvariable=self.address_var)
+        self.e3.grid(row=2, column=1)
 
         # create city variable and entry
         self.city_var = StringVar()
-        e4 = Entry(frame, textvariable=self.city_var)
-        e4.grid(row=3, column=1)
+        self.e4 = Entry(frame, textvariable=self.city_var)
+        self.e4.grid(row=3, column=1)
 
         # create state variable and entry
         self.state_var = StringVar()
-        e5 = Entry(frame, textvariable=self.state_var)
-        e5.grid(row=4, column=1)
+        self.e5 = Entry(frame, textvariable=self.state_var)
+        self.e5.grid(row=4, column=1)
 
         #create zip code variable and entry
         self.zip_var = IntVar()
-        e6 = Entry(frame, textvariable=self.zip_var)
-        e6.grid(row=5, column=1)
+        self.e6 = Entry(frame, textvariable=self.zip_var)
+        self.e6.grid(row=5, column=1)
 
         # create s.s.n variable and entry
         self.ssn_var = StringVar()
-        e4 = Entry(frame, textvariable=self.ssn_var)
-        e4.grid(row=6, column=1)
+        self.e7 = Entry(frame, textvariable=self.ssn_var)
+        self.e7.grid(row=6, column=1)
 
         # create phone variable and entry
         self.phone_var = StringVar()
-        e5 = Entry(frame, textvariable=self.phone_var)
-        e5.grid(row=7, column=1)
+        self.e8 = Entry(frame, textvariable=self.phone_var)
+        self.e8.grid(row=7, column=1)
 
         # create cell variable and entry
         self.cell_var = StringVar()
-        e6 = Entry(frame, textvariable=self.cell_var)
-        e6.grid(row=8, column=1)
+        self.e9 = Entry(frame, textvariable=self.cell_var)
+        self.e9.grid(row=8, column=1)
 
         # create quit and add buttons
         quit_button = Button(btm_frame, text="Quit", relief=GROOVE, command=parent.destroy)
@@ -175,7 +175,14 @@ class GUI:
         
 
     def callback(self):        
-        showinfo("Work_Hours_GUI", "Data Added")            
+        showinfo("Work_Hours_GUI", "Data Added")
+        self.clear_entries()
+
+    def clear_entries(self):
+        entries = [self.e1, self.e2, self.e3, self.e4, self.e5, self.e6, self.e7, self.e8, self.e9]
+        for entry in entries:
+            entry.delete(0, END)
+            
         
 
 if __name__ == '__main__':
@@ -184,21 +191,3 @@ if __name__ == '__main__':
     root.title("Work-Hours-GUI")
     mygui = GUI(root)
     root.mainloop()
-
-
-##def clear_data(self):
-##        for field in fields:
-##            field.delete(0, END)
-
-##    def match(self, filter):
-##        return filter in self.memo or filter in self.tags
-        
-
-##    def search(self, filter):
-##        return [person for person in self.people if note.match(filter)]
-
-
-##    def search_notes(self):
-##        filter = input("Search for: ")
-##        notes = self.notebook.search(filter)
-##        self.show_notes(notes)
